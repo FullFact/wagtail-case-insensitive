87a9e8b28b9f7b1c76e7b88970f069d0e2d8d8d3
Andy Lulham <a.lulham@gmail.com>
2025-04-07 21:25:09 +0100

    fix: multiple objects returned

--- case_insensitive/models.py
+++ case_insensitive/models.py
@@ -12,15 +12,14 @@ class CaseInsensitiveRoutePage(Page):
             child_slug = path_components[0]
             remaining_components = path_components[1:]
 
-            try:
-                subpage = self.get_children().get(slug__iexact=child_slug)
-                # Cache the parent page on the subpage to avoid another db query
-                # Treebeard's get_parent will use the `_cached_parent_obj` attribute if it exists
-                # And update = False
-                setattr(subpage, "_cached_parent_obj", self)
-
-            except Page.DoesNotExist:
+            subpage = self.get_children().filter(slug__iexact=child_slug).first()
+            if subpage is None:
                 raise Http404
+
+            # Cache the parent page on the subpage to avoid another db query
+            # Treebeard's get_parent will use the `_cached_parent_obj` attribute if it exists
+            # And update = False
+            setattr(subpage, "_cached_parent_obj", self)
 
             return subpage.specific.case_insensitive_route(request, remaining_components)
 
